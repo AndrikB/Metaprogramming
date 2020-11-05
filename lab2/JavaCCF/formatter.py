d354a55282f1c6dcf80af657825a0a4e540bbbb2
blagi <blagij00@gmail.com>
2020-11-06 01:07:21 +0200

    add Beginning Comments and recognize other comments

--- lab2/JavaCCF/formatter.py
+++ lab2/JavaCCF/formatter.py
@@ -1,7 +1,7 @@
 import re
 from pathlib import Path
 
-from lexer import TokenType
+from lexer import TokenType, Token, Position
 
 
 def get_files_rec(path):
@@ -23,7 +23,7 @@ def format_files(files):
     print_files(files)
 
 
-class_interface_enum = ('class', 'interface', 'enum')
+class_interface_enum = ('class', 'interface', 'enum', '@interface')
 
 
 class Formatter:
@@ -33,7 +33,7 @@ class Formatter:
 
     @staticmethod
     def get_next_no_whitespace_token_id(file, _id):
-        while _id < len(file.tokens):
+        while _id + 1 < len(file.tokens):
             _id += 1
             if file.tokens[_id].token_type != TokenType.whitespace:
                 return _id
@@ -210,10 +210,131 @@ class Formatter:
                         after_tokens = (';', '=', ':')
 
                     if (prev_token.value in ('>', ']',) or prev_token.token_type in (
-                    TokenType.identifier, TokenType.keyword)) and \
+                            TokenType.identifier, TokenType.keyword)) and \
                             next_token.value in after_tokens:
                         self.replace_to_camel_case_first_down(token)
+
+    @staticmethod
+    def insert_new_line(file, index):
+        file.tokens.insert(index, Token(TokenType.whitespace, '\n', file.tokens[index].position))
+
+    @staticmethod
+    def add_indents(file, index, indent):
+        for i in range(indent):
+            file.tokens.insert(index, Token(TokenType.whitespace, ' ', file.tokens[index + i].position))
+
+    @staticmethod
+    def fix_beginning_comment(file):
+        first_token = file.tokens[Formatter.get_next_no_whitespace_token_id(file, -1)]
+        if first_token.value[0:2] != '/*':
+            new_token = Token(TokenType.comment, '''/*
+ * %W% %E% Firstname Lastname
+ *
+ * Copyright (c) 1995-2020 %your company name%
+ *
+ * This software is the confidential and proprietary information of 
+ * %your company name%.  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with %your company name%.
+ *
+ * %your company name% MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
+ * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR
+ * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
+ * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
+ */''', Position(1, 1))
+            file.tokens.insert(0, new_token)
+            Formatter.insert_new_line(file, 1)
+
+    @staticmethod
+    def get_first_token_id_of_statement(file, index):
+        while True:
+            whitespace_id = Formatter.get_previous_token_id_with_such_value(file.tokens, index, ('\n',))
+            previous_no_whitespace_token = file.tokens[Formatter.get_prev_no_whitespace_token_id(file, whitespace_id)]
+            if not (previous_no_whitespace_token.token_type == TokenType.annotation
+                    or previous_no_whitespace_token.value == ')'):
+                return Formatter.get_next_no_whitespace_token_id(file, whitespace_id)
+            index = whitespace_id
+
+    @staticmethod
+    def fix_comment_before_class_declaration(file, class_index):
+        class_name = file.tokens[Formatter.get_next_no_whitespace_token_id(file, class_index)].second_value
+        class_type = file.tokens[class_index]
+
+        first_token_of_statement_id = Formatter.get_first_token_id_of_statement(file, class_index)
+        first_token_of_statement = file.tokens[first_token_of_statement_id]
+        indent = first_token_of_statement.position.column - 1
+
+        previous_token = file.tokens[Formatter.get_prev_no_whitespace_token_id(file, first_token_of_statement_id)]
+        if previous_token.second_value[0:3] == '/**':
+            was_new_line = previous_token.second_value[3] == '\n'  # TODO add for all
+            if class_name not in previous_token.second_value:
+                new_line = '' if was_new_line else '\n'
+                previous_token.second_value = '/**\n' + ' ' * indent + f' * The {class_name} {class_type} provides ' + \
+                                              new_line + previous_token.second_value[3:]
+            # TODO add fix end of token for all
+        else:
+            token = Token(TokenType.comment, '', first_token_of_statement.position)
+            token.second_value = '/**\n' + ' ' * indent + f' * The {class_name} {class_type} provides \n' + ' ' \
+                                 * indent + ' */'
+            file.tokens.insert(first_token_of_statement_id, token)
+            Formatter.insert_new_line(file, first_token_of_statement_id + 1)
+            Formatter.add_indents(file, first_token_of_statement_id + 2, indent)
+
+    @staticmethod
+    def fix_comment_before_method_declaration(file, method_name_index):
+        pass
+
+    @staticmethod
+    def fix_comment_before_field(file, field_name_id):
+        field_name = file.tokens[field_name_id]
+        first_token_of_statement_id = Formatter.get_first_token_id_of_statement(file, field_name_id)
+        first_token_of_statement = file.tokens[first_token_of_statement_id]
+        indent = first_token_of_statement.position.column - 1
+
+        previous_token = file.tokens[Formatter.get_prev_no_whitespace_token_id(file, first_token_of_statement_id)]
+        pass
+
+    @staticmethod
+    def fix_another_comments(file):
+        stack = []
+        i = 0
+        while i < len(file.tokens):
+            token = file.tokens[i]
+            next_token = file.tokens[Formatter.get_next_no_whitespace_token_id(file, i)]
+            if token.value in class_interface_enum and next_token.token_type == TokenType.identifier:
+                stack.append(token.value)
+                Formatter.fix_comment_before_class_declaration(file, i)
+
+            elif token.value == '{':
+                stack.append(token.value)
+            elif token.value == '}':
+                stack.pop()
+                if len(stack) > 0 and stack[-1] in class_interface_enum:
+                    stack.pop()
+            elif token.value == '(':
+                stack.append(token.value)
+            elif token.value == ')':
+                stack.pop()
+
+            if len(stack) > 1 and stack[-2] in class_interface_enum and stack[-1] == '{':  # in class
+                if token.token_type == TokenType.identifier:  # identifier (method name or value)
+
+                    if next_token.token_type == '(':  # method declaration
+                        Formatter.fix_comment_before_method_declaration(file, i)
+                    elif next_token.value in (';', '='):  # is variable
+                        Formatter.fix_comment_before_field(file, i)
+
+            i += 1
+
+    @staticmethod
+    def fix_comments(file):
+        Formatter.fix_beginning_comment(file)
+        Formatter.fix_another_comments(file)
 
     def format_files(self):
         for file in self.files:
             self.fix_names(file)
+            Formatter.fix_comments(file)
